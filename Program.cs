d33cba42760c0918ae3a85e30ec915c664f45bb7
dylan.huang <dylan.huang@ischool.com.tw>
2018-01-08 14:26:36 +0800

    新增APP線上點名專用設定器開發版

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using FISCA;
+using FISCA.Presentation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OnlineNamed
+{
+    public class Program
+    {
+        [MainMethod()]
+        static public void Main()
+        {
+            tool._A.Select<AbsenceUDT>();
+            tool._A.Select<RollCallLog>();
+
+            ServerModule.AutoManaged("http://module.ischool.com.tw/module/138/OnlineNamed/udm.xml");
+
+            RibbonBarItem Print = FISCA.Presentation.MotherForm.RibbonBarItems["學務作業", "APP線上點名"];
+            Print["點名節次缺曠別設定"].Click += delegate
+            {
+                ConfigFrom cf = new ConfigFrom();
+                cf.ShowDialog();
+
+            };
+
+
+
+
+        }
+    }
+}
